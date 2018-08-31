f9cfecc46236e62f990380a020afb496629d8652
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2018-08-31 21:58:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+/* This is a calculator app. It will have basic capabilities of a calculator
+such as adding, subtracing, division, multiplication, percentage modifier. */
+
+console.log("Hello world!");
