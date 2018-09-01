b78af4cbb0e797624448c8d693db888106a073ab
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2018-09-01 21:04:17 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 /* This is a calculator app. It will have basic capabilities of a calculator
 such as adding, subtracing, division, multiplication, percentage modifier. */
 
+function inputValue(n) {
+  let x = document.getElementById('middle').innerHTML;
+  let y = document.getElementById('lower').innerHTML;
+  document.getElementById('upper').innerHTML = x;
+  document.getElementById('middle').innerHTML = y;
+  setTimeout(function() {
+  document.getElementById('lower').innerHTML = n;
+  }, 20);
+}
+
+function plusSign() {
+  let n = document.getElementById('lower').innerHTML;
+  document.getElementById('middle').innerHTML = n;
+  document.getElementById('lower').innerHTML = "+";
+}
+
+function minusSign() {
+  let n = document.getElementById('lower').innerHTML;
+  document.getElementById('middle').innerHTML = n;
+  document.getElementById('lower').innerHTML = "-";
+}
+
+function multiplySign() {
+  let n = document.getElementById('lower').innerHTML;
+  document.getElementById('middle').innerHTML = n;
+  document.getElementById('lower').innerHTML = "x";
+}
+
+function divideSign() {
+  let n = document.getElementById('lower').innerHTML;
+  document.getElementById('middle').innerHTML = n;
+  document.getElementById('lower').innerHTML = "/";
+}
+
+function inputNine() {
+  document.getElementById('lower').innerHTML += "9";
+}
+
 function add(a, b) {
   let x = a + b;
   console.log(x);
@@ -23,6 +61,36 @@ function divide(a, b) {
   let x = a / b;
   console.log(x);
   return x;
+}
+
+function equalSign() {
+  let result;
+  let x = document.getElementById('upper').innerHTML;
+  let a = parseInt(x);
+  let y = document.getElementById('lower').innerHTML;
+  let b = parseInt(y);
+  let z = document.getElementById('middle').innerHTML;
+  if (z == "+") {
+    result = add(a, b);
+    // return result;
+  } else if (z == "-") {
+    result = subtract(a, b);
+    // return result;
+  } else if (z == "x") {
+    result = multiply(a, b);
+    // return result;
+  } else if (z == "/") {
+    result = divide(a, b);
+    // return result;
+  } else {
+    result = "error";
+    // return result;
+  }
+  setTimeout(function(){
+    document.getElementById('upper').innerHTML = "";
+    document.getElementById('middle').innerHTML = "";
+    document.getElementById('lower').innerHTML = result;
+  }, 20);
 }
 
 console.log("Hello world!");
