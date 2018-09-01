d641ab75267e3652474751e477b4004c0b90d0bf
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2018-09-01 11:51:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 /* This is a calculator app. It will have basic capabilities of a calculator
 such as adding, subtracing, division, multiplication, percentage modifier. */
 
+function add(a, b) {
+  let x = a + b;
+  console.log(x);
+  return x;
+}
+
+function subtract(a, b) {
+  let x = a - b;
+  console.log(x);
+  return x;
+}
+
+function multiply(a, b) {
+  let x = a * b;
+  console.log(x);
+  return x;
+}
+
+function divide(a, b) {
+  let x = a / b;
+  console.log(x);
+  return x;
+}
+
 console.log("Hello world!");
